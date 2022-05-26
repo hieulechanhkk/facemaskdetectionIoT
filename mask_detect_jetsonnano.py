aa0b80131e9ca864c1cf99feb932d91bcf09ef54
doanhnhantre <hieulechanhkk@gmail.com>
2022-05-26 16:53:50 +0700

    update

--- mask_detect_jetsonnano.py
+++ mask_detect_jetsonnano.py
@@ -6,10 +6,10 @@ import imutils
 # from PIL import Image
 import numpy as np
 
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 # from keras.utils.np_utils import to_categorical
-from keras.applications.mobilenet_v2 import preprocess_input
-from keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.models import load_model
 
 # from cvzone.SerialModule import SerialObject
 
@@ -54,68 +54,70 @@ flagNMask = 0
 flagMask = 0
 
 while True:
-    ret, image = vs.read()
-    image = cv2.flip(image, 1)
-    image = imutils.resize(image, width=800)
+    try:
+        ret, image = vs.read()
+        image = cv2.flip(image, 1)
+        image = imutils.resize(image, width=800)
 
-    (h, w) = image.shape[:2]
-    blob = cv2.dnn.blobFromImage(image, 1.0, (224, 224),
-                                 (104.0, 177.0, 123.0))
+        (h, w) = image.shape[:2]
+        blob = cv2.dnn.blobFromImage(image, 1.0, (224, 224),
+                                     (104.0, 177.0, 123.0))
 
-    faceNet.setInput(blob)
-    detections = faceNet.forward()
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
 
-    faces = []
-    preds = []
-    locs = []
+        faces = []
+        preds = []
+        locs = []
 
-    for i in range(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
 
-        if confidence < 0.5:
-            continue
+            if confidence < 0.5:
+                continue
 
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-        text = "{:.2f}%".format(confidence * 100)
-        face = image[startY:endY, startX:endX]
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-        face = cv2.resize(face, (224, 224))
-        face = img_to_array(face)
-        face = preprocess_input(face)
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            text = "{:.2f}%".format(confidence * 100)
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
 
-        faces.append(face)
-        locs.append((startX, startY, endX, endY))
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
 
-    if(len(faces) > 0):
-        faces = np.array(faces, dtype="float32")
-        preds = model.predict(faces, batch_size=32)
-    for (loc, pred) in zip(locs, preds):
+        if (len(faces) > 0):
+            faces = np.array(faces, dtype="float32")
+            preds = model.predict(faces, batch_size=32)
+        for (loc, pred) in zip(locs, preds):
+            (x0, y0, x1, y1) = loc
+            (with_mask, without_mask) = pred
 
-        (x0, y0, x1, y1) = loc
-        (with_mask, without_mask) = pred
+            label = "Mask" if with_mask > without_mask else "Without Mask"
+            # if with_mask > without_mask:
+            #     if (flagMask == 0):
+            #         print("Send Data Mask")  # $1
+            #         flagMask = 1
+            #         flagNMask = 0
+            #         arduino.sendData([1])
+            # else:
+            #     if (flagNMask == 0):
+            #         print("Send Data No Mask")  # $0
+            #         flagMask = 0
+            #         flagNMask = 1
+            #         arduino.sendData([0])
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+            cv2.rectangle(image, (x0, y0 - 23), (x1, y0 - 3), color, -2)
+            cv2.putText(image, label, (x0 + 5, y0 - 9), cv2.FONT_HERSHEY_SIMPLEX, 0.50, (255, 255, 255), 2)
+            cv2.rectangle(image, (x0, y0), (x1, y1), color, 2)
 
-        label = "Mask" if with_mask > without_mask else "Without Mask"
-        # if with_mask > without_mask:
-        #     if (flagMask == 0):
-        #         print("Send Data Mask")  # $1
-        #         flagMask = 1
-        #         flagNMask = 0
-        #         arduino.sendData([1])
-        # else:
-        #     if (flagNMask == 0):
-        #         print("Send Data No Mask")  # $0
-        #         flagMask = 0
-        #         flagNMask = 1
-        #         arduino.sendData([0])
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-        cv2.rectangle(image, (x0, y0 - 23), (x1, y0 - 3), color, -2)
-        cv2.putText(image, label, (x0+5, y0-9), cv2.FONT_HERSHEY_SIMPLEX, 0.50, (255, 255, 255), 2)
-        cv2.rectangle(image, (x0, y0), (x1, y1), color, 2)
-
-    cv2.imshow('Image', image)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+        cv2.imshow('Image', image)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    except:
+        print("Error")
 cv2.destroyAllWindows()
 vs.stop()
